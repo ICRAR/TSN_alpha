cc285236726e190ad152d20610738d9140962128
alex beckley <eckley@gmail.com>
2013-04-24 09:23:43 +0800

    setup emailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,10 @@ TSNAlpha::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  config.action_mailer.default_url_options = { :host => APP_CONFIG['site_host'] }
+  config.action_mailer.delivery_method = :smtp
+
+
   $statsd = Statsd.new 'localhost', 8125
   $statsd.namespace = 'TSN_dev'
 end
