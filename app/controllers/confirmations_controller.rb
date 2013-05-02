ab43c5a2bacc319a84521e73d90a2a56715429fe
alex beckley <eckley@gmail.com>
2013-05-02 14:05:50 +0800

    fixed user signup

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -3,7 +3,7 @@ class ConfirmationsController < Devise::ConfirmationsController
   private
 
   def after_confirmation_path_for(resource_name, resource)
-    url_for my_profile_path
+    url_for(:controller => 'profiles', :action => 'dashboard')
   end
 
 end
