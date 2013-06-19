18274abba7a1a4ca276aa780bb6a665f2824b444
alex beckley <eckley@gmail.com>
2013-06-19 08:16:31 +0800

    handle connect to pogs error on dashboard page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -40,7 +40,11 @@ class ProfilesController < ApplicationController
     @top_profiles = Profile.for_leader_boards_small.order("rank asc").limit(5)
     @top_alliances = Alliance.for_leaderboard_small.order('ranking asc').limit(5)
     if @profile.general_stats_item.boinc_stats_item != nil
-      @boinc_galaxy = Galaxy.find_by_user_id(@profile.general_stats_item.boinc_stats_item.boinc_id).order("galaxy.galaxy_id DESC").limit(1).first
+      begin
+        @boinc_galaxy = Galaxy.find_by_user_id(@profile.general_stats_item.boinc_stats_item.boinc_id).order("galaxy.galaxy_id DESC").limit(1).first
+      rescue Mysql2::Error => e
+        @boinc_galaxy = nil
+      end
     end
 
     case @profile.new_profile_step
