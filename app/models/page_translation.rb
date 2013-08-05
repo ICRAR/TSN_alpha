57d99de8c4a49e2355600f81b47af6c97315e7ae
alex beckley <eckley@gmail.com>
2013-08-05 16:45:50 +0800

    globalize3 rails admin

--- app/models/page_translation.rb
+++ app/models/page_translation.rb
@@ -4,4 +4,10 @@ class PageTranslation < ActiveRecord::Base
 
   belongs_to :page
   attr_accessible :locale, :title, :content, :page_attribute, as: :admin
+  rails_admin do
+    include_all_fields
+    field :content , :text do
+      ckeditor true
+    end
+  end
 end
