c8868638703680927d3f07629097c1d3b7106bce
alex beckley <eckley@gmail.com>
2013-09-23 16:16:15 +0800

    route redirect to site host

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Tsn::Application.config.session_store :cookie_store, key: '_tsn_session', :domain => APP_CONFIG['site_host']
+Tsn::Application.config.session_store :cookie_store, key: '_tsn_session', :domain => APP_CONFIG['cookie_site_host']
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
