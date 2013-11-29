0fd7c6072676fc4066d29e87cc4bb40ad2287055
alex beckley <eckley@gmail.com>
2013-11-29 09:26:55 +0800

    mail preview only in development

--- app/mailers/mail_preview.rb
+++ app/mailers/mail_preview.rb
@@ -1,22 +1,24 @@
 # app/mailers/mail_preview.rb or lib/mail_preview.rb
-class MailPreview < MailView
-  def alliance_sync_removal
-    UserMailer.alliance_sync_removal(Profile.last,Alliance.first,Alliance.last)
-  end
-  def alliance_invite
-    invite = invite = AllianceInvite.new(:email => "foo@bar.com")
-    alliance = Alliance.first
-    invite.alliance_id = alliance.id
-    invite.invited_by_id =  alliance.members.first.id
-    mail = UserMailer.alliance_invite invite
-    invite.destroy
-    mail
-  end
-  def alliance_merger_issue
-    UserMailer.alliance_merger_issue(Profile.last, Alliance.last)
-  end
-  def devise_confirmation
-    user = User.last
-    Devise::Mailer.confirmation_instructions(user, token: "token")
+if Rails.env.development?
+  class MailPreview < MailView
+    def alliance_sync_removal
+      UserMailer.alliance_sync_removal(Profile.last,Alliance.first,Alliance.last)
+    end
+    def alliance_invite
+      invite = invite = AllianceInvite.new(:email => "foo@bar.com")
+      alliance = Alliance.first
+      invite.alliance_id = alliance.id
+      invite.invited_by_id =  alliance.members.first.id
+      mail = UserMailer.alliance_invite invite
+      invite.destroy
+      mail
+    end
+    def alliance_merger_issue
+      UserMailer.alliance_merger_issue(Profile.last, Alliance.last)
+    end
+    def devise_confirmation
+      user = User.last
+      Devise::Mailer.confirmation_instructions(user, token: "token")
+    end
   end
 end
