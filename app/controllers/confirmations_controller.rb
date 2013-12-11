a90849174a015303fad7811c74d4fdbfcd51f725
alex beckley <eckley@gmail.com>
2013-12-11 12:03:33 +0800

    track successful confirmations

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -3,6 +3,7 @@ class ConfirmationsController < Devise::ConfirmationsController
   private
 
   def after_confirmation_path_for(resource_name, resource)
+    flash[:page] = "/confirmation/success"
     url_for(:controller => 'profiles', :action => 'dashboard')
   end
 
