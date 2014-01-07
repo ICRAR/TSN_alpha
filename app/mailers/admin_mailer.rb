6f4f6fb400184a0195bf1065f59f3edebf03c0e5
alex beckley <eckley@gmail.com>
2014-01-07 17:20:45 +0800

    mailer name test

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,5 +1,5 @@
 class AdminMailer < ActionMailer::Base
-  default from: "theSkyNet <#{APP_CONFIG['smtp_default_from']}>"
+  default from: APP_CONFIG['smtp_default_from']
   def debug(msg,subject)
     @body = msg
     mail to: APP_CONFIG["admin_email"], subject: subject
