4b3a60bbcbc7eaf2b3dacd17b0d60534486d427b
alex beckley <eckley@gmail.com>
2014-01-31 09:22:26 +0800

    update to form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -32,7 +32,19 @@ module ApplicationHelper
     end
   end
   def markdown(content)
-    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, space_after_headers: true, fenced_code_blocks: true)
+    @redcarpet_renderer ||= Redcarpet::Render::HTML.new(:filter_html => true,
+                                                        :hard_wrap => true)
+    @markdown ||= Redcarpet::Markdown.new(@redcarpet_renderer,
+                                          no_intra_emphasis: true,
+                                          tables: true,
+                                          strikethrough: true,
+                                          superscript: true,
+                                          underline: true,
+                                          quote: true,
+                                          autolink: true,
+                                          space_after_headers: true,
+                                          fenced_code_blocks: true,
+                                          disable_indented_code_blocks: true)
     @markdown.render(content).html_safe
   end
 end
