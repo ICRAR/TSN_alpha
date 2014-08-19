a16ebf519daa5cd94efce0e2de88b4ff88a3e048
alex beckley <eckley@gmail.com>
2014-08-19 09:18:29 +0800

    fix quadrants route for ids

--- app/controllers/the_sky_map/quadrants_controller.rb
+++ app/controllers/the_sky_map/quadrants_controller.rb
@@ -6,16 +6,16 @@ class TheSkyMap::QuadrantsController < TheSkyMap::ApplicationController
     if params[:ids]
       relation = TheSkyMap::Quadrant.where{id.in my{params[:ids]}}
     else
-      relation = TheSkyMap::Quadrant
+      x_min = params[:x_min] || 0
+      x_max = params[:x_max] || 2
+      y_min = params[:y_min] || 0
+      y_max = params[:y_max] || 2
+      z_min = params[:z_min] || 0
+      z_max = params[:z_max] || 2
+      relation = TheSkyMap::Quadrant.within_range(x_min,x_max,y_min,y_max,z_min,z_max)
     end
-    x_min = params[:x_min] || 0
-    x_max = params[:x_max] || 2
-    y_min = params[:y_min] || 0
-    y_max = params[:y_max] || 2
-    z_min = params[:z_min] || 0
-    z_max = params[:z_max] || 2
 
-    quadrants = relation.within_range(x_min,x_max,y_min,y_max,z_min,z_max).for_show(current_user.profile.the_sky_map_player)
+    quadrants = relation.for_show(current_user.profile.the_sky_map_player)
 
 
     respond_with quadrants
