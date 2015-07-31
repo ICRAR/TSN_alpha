6c13ac2e7392fd5194027dde5dab827f25f4c8da
eckley <deb6540>
2015-07-31 09:35:19 +0800

    asset compile

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ end
 module Tsn
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializersass
+    # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
