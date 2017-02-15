bd0f171d2336cab2aa7a1a70361100dd4d9c4ca0
sam6321 <sam6321@live.com.au>
2017-02-15 08:24:45 +0800

    Working on new migrate job to fix up the database's boinc/general stats entries.

--- app/models/boinc_remote_user.rb
+++ app/models/boinc_remote_user.rb
@@ -7,20 +7,9 @@ class BoincRemoteUser < BoincPogsModel
 
   has_one :profile, class_name: BoincProfile, foreign_key: 'userid'
 
-
-
-  def email_addr
-    self[:email_addr].tr(" ", "_")
-  end
-  def self.auth(login, password)
-    user = self.where{(email_addr == login)}.first
-    return false if user.nil?
-    pwd_hash = Digest::MD5.hexdigest(password+login.downcase)
-    return pwd_hash == user.passwd_hash ? user : false
-  end
-
-  #looks for a local versions of the user, if it can't find one it will create it
-  def check_local(boinc_item = nil)
+  # Creates a boinc stats item if one doesn't exist for this boinc ID.
+  # Returns the stats item associated with this boinc ID,
+  def check_stats_item(boinc_item = nil)
     # Find a local boinc stats item. Can find via the boinc_id
     boinc_item ||= BoincStatsItem.where(:boinc_id => self.id).first
     if boinc_item.nil?
@@ -35,9 +24,11 @@ class BoincRemoteUser < BoincPogsModel
       puts "Boinc stats item already exists for #{self.email_addr}"
     end
 
-    #check if the users are already joined if so do nothing
-    # return true unless boinc_item.general_stats_item_id.nil?
+    boinc_item
+  end
 
+  # Returns a local user with the same email address as the remote user, or nil of no local users with that email exist.
+  def find_local_by_email
     # Look for a local user by their email address
     email_encoded = self.email_addr
     begin
@@ -48,19 +39,67 @@ class BoincRemoteUser < BoincPogsModel
     end
 
     local_user = User.where{email == my{email_encoded}}.first
-
-    # If there's no local user, create them
     if local_user.nil?
-      puts "Copying user #{self.email_addr}to local..."
-      self.copy_to_local(self.passwd_hash,false)
-    elsif local_user.profile.general_stats_item.boinc_stats_item.nil?
-      puts "Linking users #{self.email_addr} with #{boinc_item.id}"
+      local_user = User.where{boinc_id == my{self.id}}
+    end
+
+    local_user
+  end
+
+  # Patches the links between a local user and their boinc item.
+  def patch_links(local_user, boinc_item)
+    if local_user.profile.general_stats_item.boinc_stats_item.nil?
       # If there is a local user, link their data together
       local_user.profile.general_stats_item.boinc_stats_item = boinc_item
       local_user.profile.general_stats_item.update_credit
-      self.email_addr
+      email_addr
+      true
+    end
+    false
+  end
+
+  def email_addr
+    self[:email_addr].tr(" ", "_")
+  end
+
+  def self.auth(login, password)
+    user = self.where{(email_addr == login)}.first
+    return false if user.nil?
+    pwd_hash = Digest::MD5.hexdigest(password+login.downcase)
+    return pwd_hash == user.passwd_hash ? user : false
+  end
+
+  #looks for a local versions of the user, if it can't find one it will create it
+  def check_local(boinc_item = nil)
+    local_user = User.where("boinc_id = #{self.id}").first
+
+    # Get or create a boinc stats item for this boinc id
+    boinc_item = check_stats_item(boinc_item)
+
+    if local_user != nil
+      # A user with this Boinc ID already exists locally, try to patch up any local links
+      if patch_links(local_user, boinc_item)
+        puts "User #{self.email_addr} with bad stats item patched"
+      end
+
+      return
+    end
+
+    # No local user by boinc ID
+
+    # See if we can instead find them by email
+    local_user = find_local_by_email
+
+    # If there's no local user by email or boinc id, create them
+    if local_user.nil?
+      puts "Copying user #{self.email_addr}to local..."
+      copy_to_local(self.passwd_hash,false)
+    elsif patch_links(local_user, boinc_item)
+      # Found a user with an email address matching, but no boinc id matching.
+      puts "Linking user #{self.email_addr} with #{self.id}"
+      local_user.boinc_id = self.id
     else
-      puts "User is ok"
+      puts 'User is ok'
     end
 
   end
