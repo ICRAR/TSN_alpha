a58b64417ee99c25fb52ee9003daffa152549287
eckley <deb6540>
2017-06-24 11:32:25 +0800

    disable search engine for now :(

--- config/initializers/tire.rb
+++ config/initializers/tire.rb
@@ -1,3 +1,4 @@
 Tire.configure do
-  url(APP_CONFIG['elastic_search_host'])  if  APP_CONFIG['elastic_search_host']
+#  url(APP_CONFIG['elastic_search_host'])  if  APP_CONFIG['elastic_search_host']
+   url("http://localhost:9200")
 end
