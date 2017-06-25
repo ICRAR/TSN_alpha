ebda9328f1e463687c859a1042eb462cc172a29b
eckley <deb6540>
2017-06-25 11:20:04 +0800

    Re add search engine
    Signed-off-by: eckley <deb6540>

--- config/initializers/tire.rb
+++ config/initializers/tire.rb
@@ -1,4 +1,4 @@
 Tire.configure do
-#  url(APP_CONFIG['elastic_search_host'])  if  APP_CONFIG['elastic_search_host']
-   url("http://localhost:9200")
+  url(APP_CONFIG['elastic_search_host'])  if  APP_CONFIG['elastic_search_host']
+#   url("http://localhost:9200")
 end
